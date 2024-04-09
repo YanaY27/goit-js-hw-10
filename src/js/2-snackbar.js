dde7925a19ea613598ded7ccca3a54ade35f6d4f
YanaY27 <yanayeromina@gmail.com>
2024-04-09 17:58:01 +0300

    upd

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = +this.elements.delay.value;
+
+  const state = this.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
